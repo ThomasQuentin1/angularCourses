f2a05cf6fbf7b755bb97d933263f326bec11d1b3
SillyDEV1 <raphael.giannone@epitech.eu>
2021-02-20 13:57:37 +0100

    Version stable des pages en dur

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+  src: string;
+  name: string;
+}
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
-
+  tiles: Tile[] = [
+    {text: 'Welcome everybody in our blog, you will enjoy with us the passion for ..... We are a team of 4 students from Epitech and we have a tons of ... to present to you', cols: 4, rows: 1, color: ' #cf024f ', src: '', name: ''},
+    {text: '', cols: 1, rows: 1, color: 'lightgreen', src: 'assets/quentin1.thomas.jpg', name: 'a'},
+    {text: '', cols: 1, rows: 1, color: 'lightpink', src:'assets/raphael.giannone.jpg', name: 'b'},
+    {text: '', cols: 1, rows: 1, color: '#DDBDF1', src: 'assets/arthur.de-rosny.jpg', name: 'b'},
+    {text: '', cols: 1, rows: 1, color: '#f3ff77 ', src: 'assets/tom.florentin.jpg', name: 'c'},
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
-export class ContactPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ContactPageComponent {
+  
 
 }
