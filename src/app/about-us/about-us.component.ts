40afc43ed890c37980f7f4b6f8ce6e76ccf94b81
SillyDEV1 <raphael.giannone@epitech.eu>
2021-02-21 21:04:11 +0100

    d

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -15,11 +15,11 @@ export interface Tile {
 })
 export class AboutUsComponent implements OnInit {
   tiles: Tile[] = [
-    {text: 'Welcome everybody in our blog, you will enjoy with us the passion for ..... We are a team of 4 students from Epitech and we have a tons of ... to present to you', cols: 4, rows: 1, color: ' #cf024f ', src: '', name: ''},
-    {text: '', cols: 1, rows: 1, color: 'lightgreen', src: 'assets/quentin1.thomas.jpg', name: 'a'},
-    {text: '', cols: 1, rows: 1, color: 'lightpink', src:'assets/raphael.giannone.jpg', name: 'b'},
-    {text: '', cols: 1, rows: 1, color: '#DDBDF1', src: 'assets/arthur.de-rosny.jpg', name: 'b'},
-    {text: '', cols: 1, rows: 1, color: '#f3ff77 ', src: 'assets/tom.florentin.jpg', name: 'c'},
+    {text: 'We are a team of 4 people managing Bloggy ! Enjoy our passion as much as we enjoy it !', cols: 4, rows: 0.5, color: ' #cf024f ', src: '', name: ''},
+    {text: '', cols: 1, rows: 1, color: 'lightgreen', src: 'assets/quentin1.thomas.jpg', name: ''},
+    {text: '', cols: 1, rows: 1, color: 'lightpink', src:'assets/raphael.giannone.jpg', name: ''},
+    {text: '', cols: 1, rows: 1, color: '#DDBDF1', src: 'assets/arthur.de-rosny.jpg', name: ''},
+    {text: '', cols: 1, rows: 1, color: '#f3ff77 ', src: 'assets/tom.florentin.jpg', name: ''},
   ];
   constructor() { }
 
