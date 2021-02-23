d587ce0653b342b3a6143d3cf7106ff57eb32c33
SillyDEV1 <raphael.giannone@epitech.eu>
2021-02-23 15:56:56 +0100

    Author added

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -15,11 +15,11 @@ export interface Tile {
 })
 export class AboutUsComponent implements OnInit {
   tiles: Tile[] = [
-    {text: 'We are a team of 4 people managing Bloggy ! Enjoy our passion as much as we enjoy it !', cols: 4, rows: 0.5, color: ' #cf024f ', src: '', name: ''},
-    {text: '', cols: 1, rows: 1, color: 'lightgreen', src: 'assets/quentin1.thomas.jpg', name: ''},
-    {text: '', cols: 1, rows: 1, color: 'lightpink', src:'assets/raphael.giannone.jpg', name: ''},
-    {text: '', cols: 1, rows: 1, color: '#DDBDF1', src: 'assets/arthur.de-rosny.jpg', name: ''},
-    {text: '', cols: 1, rows: 1, color: '#f3ff77 ', src: 'assets/tom.florentin.jpg', name: ''},
+    {text: 'We are a team of 4 people managing Bloggy ! Enjoy our passion as much as we enjoy it !', cols: 4, rows: 0.5, color: ' #2a9d8f ', src: '', name: ''},
+    {text: '', cols: 1, rows: 1, color: '#f4a261', src: 'assets/quentin1.thomas.jpg', name: ''},
+    {text: '', cols: 1, rows: 1, color: '#264653', src:'assets/raphael.giannone.jpg', name: ''},
+    {text: '', cols: 1, rows: 1, color: '#e9c46a', src: 'assets/arthur.de-rosny.jpg', name: ''},
+    {text: '', cols: 1, rows: 1, color: '#e76f51 ', src: 'assets/tom.florentin.jpg', name: ''},
   ];
   constructor() { }
 
